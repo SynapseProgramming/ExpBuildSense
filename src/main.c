c8855a428ee69f632b89e57f879029ffbf6eceb8
Roald Ong <roald.ong@gmail.com>
2022-07-03 20:27:13 +0800

    First working version of acc sensor with getX

--- src/main.c
+++ src/main.c
@@ -7,27 +7,120 @@
 #include "driver/gpio.h"
 #include "esp_log.h"
 #include "sdkconfig.h"
+#include "driver/i2c.h"
 
-#define V_REF 1700
+static const char *TAG = "i2c-simple-example";
 
-// ADC1_CHANNEL_4 is pin 32
+#define I2C_MASTER_SCL_IO 26        /*!< GPIO number used for I2C master clock */
+#define I2C_MASTER_SDA_IO 27        /*!< GPIO number used for I2C master data  */
+#define I2C_MASTER_NUM 0            /*!< I2C master i2c port number, the number of i2c peripheral interfaces available will depend on the chip */
+#define I2C_MASTER_FREQ_HZ 400000   /*!< I2C master clock frequency */
+#define I2C_MASTER_TX_BUF_DISABLE 0 /*!< I2C master doesn't need buffer */
+#define I2C_MASTER_RX_BUF_DISABLE 0 /*!< I2C master doesn't need buffer */
+#define I2C_MASTER_TIMEOUT_MS 1000
+
+#define BMA220_SENSOR_ADDR 0x0A        /*!< Slave address of the BMA220 sensor */
+#define MPU9250_WHO_AM_I_REG_ADDR 0x75 /*!< Register addresses of the "who am I" register */
+
+#define MPU9250_PWR_MGMT_1_REG_ADDR 0x6B /*!< Register addresses of the power managment register */
+#define MPU9250_RESET_BIT 7
+
+/**
+ * @brief Read a sequence of bytes from a MPU9250 sensor registers
+ */
+// static esp_err_t mpu9250_register_read(uint8_t reg_addr, uint8_t *data, size_t len)
+// {
+//     return i2c_master_write_read_device(I2C_MASTER_NUM, BMA220_SENSOR_ADDR, &reg_addr, 1, data, len, I2C_MASTER_TIMEOUT_MS / portTICK_PERIOD_MS);
+// }
+
+/**
+ * @brief Write a byte to a MPU9250 sensor register
+ */
+
+// static esp_err_t mpu9250_register_write_byte(uint8_t reg_addr, uint8_t data)
+// {
+//     int ret;
+//     uint8_t low_pass_filter = 0x20;
+//     uint8_t filter_config = 0x05;
+//     uint8_t write_buf[2] = {low_pass_filter,filter_config};
+
+//     ret = i2c_master_write_to_device(I2C_MASTER_NUM, BMA220_SENSOR_ADDR, write_buf, sizeof(write_buf), I2C_MASTER_TIMEOUT_MS / portTICK_PERIOD_MS);
+
+//     return ret;
+// }
+
+// function to initialise the BMA220 accelerometer sensor
+static esp_err_t init_BMA220()
+{
+    int err;
+
+    uint8_t low_pass_filter = 0x20;
+    uint8_t filter_config = 0x05;
+    uint8_t write_buf[2] = {low_pass_filter, filter_config};
+
+    err = i2c_master_write_to_device(I2C_MASTER_NUM, BMA220_SENSOR_ADDR, write_buf, sizeof(write_buf), I2C_MASTER_TIMEOUT_MS / portTICK_PERIOD_MS);
+    return err;
+}
+
+/**
+ * @brief i2c master initialization
+ */
+static esp_err_t i2c_master_init(void)
+{
+    int i2c_master_port = I2C_MASTER_NUM;
+
+    i2c_config_t conf = {
+        .mode = I2C_MODE_MASTER,
+        .sda_io_num = I2C_MASTER_SDA_IO,
+        .scl_io_num = I2C_MASTER_SCL_IO,
+        .sda_pullup_en = GPIO_PULLUP_ENABLE,
+        .scl_pullup_en = GPIO_PULLUP_ENABLE,
+        .master.clk_speed = I2C_MASTER_FREQ_HZ,
+    };
+
+    i2c_param_config(i2c_master_port, &conf);
+
+    return i2c_driver_install(i2c_master_port, conf.mode, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0);
+}
+
+// function to get the X value of the BMA220 sensor. Must be called after BMA220_init
+// argument would contain new x value
+
+static esp_err_t BMA220_getX(uint8_t *x_value)
+{
+    uint8_t x_command = 0x04;
+    // shift bits to the right to account for offset
+    uint8_t shifted_x_value = 0;
+    esp_err_t err;
+    err = i2c_master_write_read_device(I2C_MASTER_NUM, BMA220_SENSOR_ADDR, &x_command, 1, &shifted_x_value, 1, I2C_MASTER_TIMEOUT_MS / portTICK_PERIOD_MS);
+    *x_value = (int8_t)shifted_x_value >> 2;
+    return err;
+}
 
 void app_main(void)
 {
+    // uint8_t data[2];
+    ESP_ERROR_CHECK(i2c_master_init());
+    ESP_LOGI(TAG, "I2C initialized successfully");
 
-    adc1_config_width(ADC_WIDTH_BIT_12);
-    adc1_config_channel_atten(ADC1_CHANNEL_4, ADC_ATTEN_DB_11);
+    /* Read the MPU9250 WHO_AM_I register, on power up the register should have the value 0x71 */
+    // ESP_ERROR_CHECK(mpu9250_register_read(MPU9250_WHO_AM_I_REG_ADDR, data, 1));
+    // ESP_LOGI(TAG, "WHO_AM_I = %X", data[0]);
 
-    // Calculate ADC characteristics i.e. gain and offset factors
-    esp_adc_cal_characteristics_t characteristics;
-    esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, 0, &characteristics);
+    ESP_ERROR_CHECK(init_BMA220());
 
+    /* Demonstrate writing by reseting the MPU9250 */
+    // ESP_ERROR_CHECK(mpu9250_register_write_byte(MPU9250_PWR_MGMT_1_REG_ADDR, 1 << MPU9250_RESET_BIT));
+    // get current x of the accelerometer
+    uint8_t x_val = 0;
     while (1)
     {
 
-        uint32_t val = adc1_get_raw(ADC1_CHANNEL_4);
-        uint32_t  voltage = esp_adc_cal_raw_to_voltage(val, &characteristics);
-        ESP_LOGI("current", "ADC in mV %d", voltage);
-        vTaskDelay(100 / portTICK_PERIOD_MS);
+        BMA220_getX(&x_val);
+        ESP_LOGI(TAG, "X value: %d", x_val);
+        vTaskDelay(500 / portTICK_PERIOD_MS);
     }
+
+    ESP_ERROR_CHECK(i2c_driver_delete(I2C_MASTER_NUM));
+    ESP_LOGI(TAG, "I2C de-initialized successfully");
 }
