05df9b47a2a3ef65c4b451a03b3f608f54b81dd4
Roald Ong <roald.ong@gmail.com>
2022-08-27 00:09:25 +0800

    Output battery voltage

--- src/main.c
+++ src/main.c
@@ -9,6 +9,8 @@
 #include "sdkconfig.h"
 
 #define V_REF 1700
+#define BATTERY_MAX 3200 // maximum voltage of battery in mV
+#define BATTERY_MIN 2600 // minimum voltage of battery in mV
 
 // ADC1_CHANNEL_4 is pin 32
 
@@ -22,12 +24,22 @@ void app_main(void)
     esp_adc_cal_characteristics_t characteristics;
     esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, 0, &characteristics);
 
+    double scaled_voltage = 0;
+
     while (1)
     {
 
         uint32_t val = adc1_get_raw(ADC1_CHANNEL_4);
-        uint32_t  voltage = esp_adc_cal_raw_to_voltage(val, &characteristics);
-        ESP_LOGI("current", "ADC in mV %d", voltage);
+        uint32_t voltage = esp_adc_cal_raw_to_voltage(val, &characteristics);
+        // limit voltage between max and min range
+        if (voltage >= BATTERY_MAX)
+            voltage = BATTERY_MAX;
+        if (voltage <= BATTERY_MIN)
+            voltage = BATTERY_MIN;
+
+        scaled_voltage = (1.0 / 6.0) * voltage - 433.333333;
+
+        ESP_LOGI("current", "battery: %f", scaled_voltage);
         vTaskDelay(100 / portTICK_PERIOD_MS);
     }
 }
